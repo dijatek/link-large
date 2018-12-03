7d550e35dab3f624819348cd861859b59d42a0a0
Dan K <KC135Q@users.noreply.github.com>
2018-12-02 21:36:12 -0500

    Need to put error check on incorrect login

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,10 +5,10 @@ var path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -17,8 +17,12 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then((dbExample) => {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then((dbExample) => {
       res.render("example", {
         example: dbExample
       });
@@ -34,7 +38,7 @@ module.exports = function(app) {
   //   res.sendFile(path.join(__dirname, "../public/signup.html"));
   // });
 
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
@@ -42,7 +46,7 @@ module.exports = function(app) {
     res.render("login");
   });
 
-  app.get("/signup", function(req, res) {
+  app.get("/signup", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
@@ -52,12 +56,12 @@ module.exports = function(app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
+  app.get("/members", isAuthenticated, function (req, res) {
     res.render("members");
   });
 
-    // Render 404 page for any unmatched routes
-    app.get("*", function(req, res) {
-      res.render("404");
-    });
+  // Render 404 page for any unmatched routes
+  app.get("*", function (req, res) {
+    res.render("404");
+  });
 };
